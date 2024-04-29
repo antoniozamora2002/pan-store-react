e62a4385ae3192778d052367d5cfd57287d2210c
antoniozamora2002 <antonio.zamora@upeu.edu.pe>
2024-04-29 14:42:21 -0500

    Agrega proyecto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+  import logo from './logo.svg';
+  import './App.css';
+  import 'bootstrap/dist/css/bootstrap.min.css';
+  import { Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
+  import { Component } from 'react';
+  import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const url = "https://panaderia.informaticapp.com/proveedores";
 
-export default App;
+  class App extends Component{
+    state = {
+      data:[]
+    }
+
+    peticionGet=()=>{
+      axios.get(url,{
+        headers: {
+          'Authorization': 'Basic YTJhYTA3YWRmaGRmcmV4ZmhnZGZoZGZlcnR0Z2ViYkNQeXhzT3F1LlFMbVdFNFhiaVVlbjZpUEVtSU4uOm8yYW8wN29kZmhkZnJleGZoZ2RmaGRmZXJ0dGdlUzQ0ZkFhejRMUWFUdFdRNVdnakc1c1g4ZE5vYk5GaQ=='
+        }
+      }).then(response=>{
+        console.log(response.data);
+        this.setState({ data: response.data });
+      }).catch(error=>{
+        console.log(error);
+      });
+    }
+
+    componentDidMount(){
+      this.peticionGet();
+    }
+
+    render() {
+      return (
+      <div className="App">
+        <br /><br /><br />
+        <button className="btn btn-success">Agregar Empresa</button>
+        <br /><br />
+        <table className="table ">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Razon Social</th>
+              <th>RUC</th>
+              <th>Nombre Encargado</th>
+              <th>Apellido Encargado</th>
+              <th>Telefono</th>
+              <th>Dirección</th>
+              <th>Empresa</th>
+              <th>Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+  }
+
+  export default App;
---
+++ src/ConexionAPI.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ConexionAPI = () => {
+  return (
+    <div>ConexionAPI</div>
+  )
+}
+
+export default ConexionAPI
